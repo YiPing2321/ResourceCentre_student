0cf769bb3cd0fc027f8b09dccf194a285fb807cc
19009259 <19009259@DESKTOP-VF487TE>
2020-07-27 15:14:57 +0800

    added retrieveallchromebook

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -146,7 +146,7 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here
-		//XinRu
+		//XinRu .
 		for (int i = 0; i <chromebookList.size(); i ++) {
 		      output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
 		          chromebookList.get(i).getDescription(), 

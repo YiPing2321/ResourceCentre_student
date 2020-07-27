897f3cdd9310de1688dde76b7193a9208b970f09
19009259 <19009259@DESKTOP-VF487TE>
2020-07-27 14:34:28 +0800

    added doReturnCamcorder

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -127,7 +127,7 @@ public class ResourceCentreTest {
 		//XinRu
 		//fail("Not yet implemented");
 		// write your code here
-		//checked for valid camcorder arraylist to return to
+		//checked for valid Camcorder arraylist to return to
 		assertNotNull("Test if there is valid Camcorder arraylist to return to", camcorderList);
 		ResourceCentre.doReturnCamcorder(camcorderList, cc1.getAssetTag());
 		assertTrue("Test if item has been returned", cc1.getIsAvailable());

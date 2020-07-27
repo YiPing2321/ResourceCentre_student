54ecdec027f213fcee454ec561ae3f7c898ec341
19030407 <19030407@LAPTOP-2RBNFU3F>
2020-07-27 15:18:41 +0800

    Update

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -261,19 +261,12 @@ public class ResourceCentre {
 		String tag = Helper.readString("Enter asset tag > ");
 		String due = Helper.readString("Enter due date > ");
 		
-<<<<<<< HEAD
-		ResourceCentre.viewAllChromebook(chromebookList);
-		String tag = Helper.readString("Enter asset tag > ");
-		String due = Helper.readString("Enter due date > ");
-		Boolean isLoaned =doLoanChromebook(chromebookList, tag, due);
-=======
 		Boolean isLoaned = doLoanChromebook(chromebookList, tag, due);
->>>>>>> branch 'master' of https://github.com/YiPing2321/ResourceCentre_student.git
 		if (isLoaned == false) {
 			System.out.println("Invalid asset tag");
 		} else {
 			System.out.println("Chromebook " + tag + " loaned out");
-		}
+        }
 	}
 	
 	//================================= Option 4 Return =================================
